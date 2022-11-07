1ed12df9dc2cb3b021aa1b82bb9d64e0bf8abbb9
numa <numamoondev@gmail.com>
2022-11-07 15:13:44 +0500

    test: unit

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,9 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   moduleNameMapper: {
     "test/(.*)": "<rootDir>/test/$1",
   },
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.{js,ts}"],
+  // collectCoverage: true,
+  // collectCoverageFrom: ["src/**/*.{js,ts}"],
 };
